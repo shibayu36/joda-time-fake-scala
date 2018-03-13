26610e92d97b655b8f7d2001f305f09edc0383ff
shibayu36 <shibayu36@gmail.com>
2018-03-14 04:20:51 +0900

    Implements fakeWithTimer

--- src/main/scala/org/shibayu36/jodatimefake/FakeTimer.scala
+++ src/main/scala/org/shibayu36/jodatimefake/FakeTimer.scala
@@ -1,14 +1,15 @@
 package org.shibayu36.jodatimefake
 
-import org.joda.time.{ DateTime, DateTimeUtils }
+import org.joda.time.{DateTime, DateTimeUtils}
 
 object FakeTimer {
-  /**
-   * Fix DateTime.now by specified timeMillis.
-   * Restored after block.
+  /** Fake DateTime.now by specified timeMillis.
+   *
+   * Time will be restored after block.
    */
   def fake[T](timeMillis: Long)(block: => T): T = {
-    DateTimeUtils.setCurrentMillisFixed(timeMillis)
+    val timer = new FakeTimer(timeMillis)
+    DateTimeUtils.setCurrentMillisProvider(new FakeTimerMillisProvider(timer))
     try {
       block
     } finally {
@@ -16,16 +17,52 @@ object FakeTimer {
     }
   }
 
-  /**
-   * Fix DateTime.now by specified DateTime object.
-   */
+  /** Fake DateTime.now by specified DateTime object. */
   def fake[T](t: DateTime)(block: => T): T =
     fake(t.getMillis)(block)
 
-  /**
-   * Fix DateTime.now by specified ISODateTimeFormat string
+  /** Fake DateTime.now by specified ISODateTimeFormat string
+   *
    * e.g. TimeFaker.fake("2018-03-01T12:34:56") { }
    */
   def fake[T](t: String)(block: => T): T =
     fake(DateTime.parse(t).getMillis)(block)
+
+  /** Fake DateTime.now by specified timeMillis.
+   *
+   * This method passes FakeTimer instance to block,
+   * so you can advance time by tick method.
+   */
+  def fakeWithTimer[T](timeMillis: Long)(block: FakeTimer => T): T = {
+    val timer = new FakeTimer(timeMillis)
+    DateTimeUtils.setCurrentMillisProvider(new FakeTimerMillisProvider(timer))
+    try {
+      block(timer)
+    } finally {
+      DateTimeUtils.setCurrentMillisSystem()
+    }
+  }
+
+  /** DateTime object version of fakeWithTimer */
+  def fakeWithTimer[T](t: DateTime)(block: FakeTimer => T): T =
+    fakeWithTimer(t.getMillis)(block)
+
+  /** ISODateTimeFormat string version of fakeWithTimer */
+  def fakeWithTimer[T](t: String)(block: FakeTimer => T): T =
+    fakeWithTimer(DateTime.parse(t).getMillis)(block)
+}
+
+class FakeTimer(_currentMillis: Long) {
+  private[this] var currentMillis = _currentMillis
+
+  /** Advance time by millis */
+  def tick(millis: Long): Unit = {
+    currentMillis = currentMillis + millis
+  }
+
+  private[jodatimefake] def getMillis(): Long = currentMillis
+}
+
+private[jodatimefake] class FakeTimerMillisProvider(timer: FakeTimer) extends DateTimeUtils.MillisProvider {
+  def getMillis(): Long = timer.getMillis
 }
--- src/test/scala/org/shibayu36/jodatimefake/FakeTimerSpec.scala
+++ src/test/scala/org/shibayu36/jodatimefake/FakeTimerSpec.scala
@@ -6,7 +6,7 @@ import org.joda.time.{DateTime, DateTimeZone}
 class FakeTimerSpec extends FunSpec with Matchers {
   val tz = DateTimeZone.forID("Asia/Tokyo")
 
-  describe("TimeFaker.fake") {
+  describe("FakeTimer.fake") {
     it("Fix time only in the block") {
       FakeTimer.fake(1515974400000L) {
         DateTime.now(tz).toString shouldBe "2018-01-15T09:00:00.000+09:00"
@@ -39,6 +39,45 @@ class FakeTimerSpec extends FunSpec with Matchers {
     it("ISODateTimeFormat string can be passed") {
       FakeTimer.fake("2018-03-02T12:34:56+09:00") {
         DateTime.now(tz).toString shouldBe "2018-03-02T12:34:56.000+09:00"
+      }
+    }
+  }
+
+  describe("FakeTimer.fakeWithTimer") {
+    it("Fake time by millis with timer") {
+      val result = FakeTimer.fakeWithTimer(1515974400000L) { t =>
+        DateTime.now(tz).toString shouldBe "2018-01-15T09:00:00.000+09:00"
+        t.tick(100)
+        DateTime.now(tz).toString shouldBe "2018-01-15T09:00:00.100+09:00"
+        t.tick(1000)
+        DateTime.now(tz).toString shouldBe "2018-01-15T09:00:01.100+09:00"
+        t.tick(3600 * 1000)
+        DateTime.now(tz).toString shouldBe "2018-01-15T10:00:01.100+09:00"
+
+        DateTime.now(tz).toString
+      }
+
+      // Last value of block is returned
+      result shouldBe "2018-01-15T10:00:01.100+09:00"
+
+      // Restore after block
+      DateTime.now(tz).toString shouldNot be(result)
+    }
+
+    it("Fake time by DateTime with timer") {
+      val dt = new DateTime(2018, 2, 13, 14, 59, tz)
+      FakeTimer.fakeWithTimer(dt) { t =>
+        DateTime.now(tz).toString shouldBe "2018-02-13T14:59:00.000+09:00"
+        t.tick(2000)
+        DateTime.now(tz).toString shouldBe "2018-02-13T14:59:02.000+09:00"
+      }
+    }
+
+    it("Fake time by ISODateTimeFormat string with timer") {
+      FakeTimer.fakeWithTimer("2018-03-02T12:34:56+09:00") { t =>
+        DateTime.now(tz).toString shouldBe "2018-03-02T12:34:56.000+09:00"
+        t.tick(60 * 1000)
+        DateTime.now(tz).toString shouldBe "2018-03-02T12:35:56.000+09:00"
       }
     }
   }
